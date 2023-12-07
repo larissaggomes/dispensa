a71671cc6545956177aaff67b1ebea29a1c27274
larissaggomes <larissgomes92@gmail.com>
2023-12-07 17:46:51 -0400

    feat: adiconando editar

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ const salvarProduto = () => {
     let preco = document.getElementById('preco');
     let id = document.getElementById('id');
     if (nome.value && preco.value && id.value) {
-        cadastrar(id.value,nome.value,preco.value)
-        
+        cadastrar(id.value, nome.value, preco.value)
+
         nome.value = "";
         preco.value = "";
         id.value = "";
@@ -15,44 +15,128 @@ const salvarProduto = () => {
 
 }
 
-const cadastrar = (id,nome,preco) =>{
+const cadastrar = (id, nome, preco) => {
     // POST request using fetch()
-fetch("http://localhost:4000/produtos", {
-     
-// Adding method type
-method: "POST",
- 
-// Adding body or contents to send
-body: JSON.stringify({
-    id: id,
-    nome: nome,
-    preco: preco
-}),
- 
-// Adding headers to the request
-headers: {
-    "Content-type": "application/json; charset=UTF-8"
+    fetch("http://localhost:4000/produtos", {
+
+        // Adding method type
+        method: "POST",
+
+        // Adding body or contents to send
+        body: JSON.stringify({
+            id: id,
+            nome: nome,
+            preco: preco
+        }),
+
+        // Adding headers to the request
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+
+        // Converting to JSON
+        .then(response => response.json())
+
+        // Displaying results to console
+        .then(json => console.log(json));
 }
-})
 
-// Converting to JSON
-.then(response => response.json())
+const editar = (id, nome, preco) => {
+    // criando o objeto do json do payload(corpo que leva os dados)
+    payload = JSON.stringify({
+        nome: nome,
+        preco: preco
+    })
 
-// Displaying results to console
-.then(json => console.log(json));
+    // contatenando o id na url para fazer o PUT
+    fetch("http://localhost:4000/produtos/" + id, {
+
+        // chamando o metodo PUT serve para fazer atualizaçao
+        method: "PUT",
+
+        // adiconado o payload ao corpo da requisiçao 
+        body: payload,
+
+        // Adding headers to the request
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+
+        // Converting to JSON
+        .then(response => response.json())
+
+        // Displaying results to console
+        .then(json => console.log(json));
 }
 
 const ler = () => {
+    // pegando o tbody da tela 
+    tbody = document.querySelector('tbody')
+
+    //faz uma requisao na url da API e usando o GET para pegar uma lista de objetos
     fetch("http://localhost:4000/produtos")
 
-        // Converting received data to JSON
+        // Convetendo os dados (lista de produtos) de texto json para objeto json
         .then(response => response.json())
         .then(json => {
-            console.log(json);
+            json.forEach(produto => {
+                // criando novos elemntos como o tr,th e tdnome tdpreco
+                tr = document.createElement('tr')
+                th = document.createElement('th')
+                td_nome = document.createElement('td')
+                td_preco = document.createElement('td')
+                td_editar = document.createElement('td')
+                td_deletar = document.createElement('td')
+
+                //inserido o valor do scope
+                th.scope = 'row'
+                //inserido o texto id,nome e preço
+                th.innerText = produto.id
+                td_nome.innerText = produto.nome
+                td_preco.innerText = produto.preco
+                td_editar.innerText = "e"
+                td_deletar.innerText = "d"
+                td_editar.onclick = () => {
+                    window.location.replace("index.html?id=" + produto.id)
+                }
+                td_deletar.onclick = () => {
+                    console.log(produto.id);
+                }
+
+                //inserido os filhos do elemento do tr
+                tr.append(th)
+                tr.append(td_nome)
+                tr.append(td_preco)
+                tr.append(td_editar)
+                tr.append(td_deletar)
+
+                //adicionado ao tbody o elemento tr 
+                tbody.append(tr)
+            });
 
         });
 }
 
+const getbyid = () => {
+
+
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var id_produto = url.searchParams.get("id"); //pega o value
+
+    fetch("http://localhost:4000/produtos/"+id_produto)
+
+        // Convetendo os dados (lista de produtos) de texto json para objeto json
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+        })
+
+}
+
 window.addEventListener('DOMContentLoaded', event => {
     ler()
+    getbyid()
 });
