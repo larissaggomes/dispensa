a266a24b2679ef4cbc33b171bb29754c909143bc
larissaggomes <larissgomes92@gmail.com>
2023-12-20 11:11:30 -0400

    feat: adicionado botao

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,60 @@
-const salvarProduto = () => {
-    //pegendo os elementos input da tela
+const getbyid = () => {
+    const id_produto = getidbyURL();
+
+    //validando se existe uma id na url
+    if (!id_produto)return false;
+        
+    
+    //pegando os elementos da tela por id
     const inputNome = document.getElementById('nome');
     const inputPreco = document.getElementById('preco');
-    const inputId = document.getElementById('id');
 
-    //verificando se os valores dos inputs sao validos
-    if (nome.value && preco.value && id.value) {
-        //pegando o id da url
-        const id_produto = getidbyURL();
+    
+    //fazendo uma requisição para api passando o id do produto no final da url
+    //para buscaro produto por id no banco de dados
+    fetch("http://localhost:4000/produtos/"+id_produto)
 
-        //verificando se existe um id na url
-        (id_produto) ? 
-        //se sim editamos 
-        editar(inputId.value, inputNome.value, inputPreco.value)
-        //se nao, cadastramos 
-        :cadastrar(inputId.value, inputNome.value, inputPreco.value);
+        // Convetendo os dados (lista de produtos) de texto json para objeto json
+        .then(response => response.json())
+        .then(produto => {
 
-        
-        
-    } else {
-        //caso os campos estejam invalidos, mostrar mensagem na tela
-        alert("Campos obrigatórios");
-    }
-
+            //inserido os valores dos inputs de acordo com o produto
+            // recebido da requisição
+            inputNome.value = produto.nome;
+            inputPreco.value = produto.preco;
+        })
 
 }
+
+const getidbyURL = () => {
+    //pegando a url atual
+    const url_string = window.location.href;
+    //coventendo a string de url em um objeto URL
+    const url = new URL(url_string);
+    //pegando o parametro ID da url
+    const id_produto = url.searchParams.get("id");
+
+    //retornado id de produto
+    return id_produto;
+} 
 
 const ler = () => {
     // pegando o tbody da tela 
     const tbody = document.querySelector('tbody');
+    
+    //validando o tbody
+    if(!tbody)return false;
+
+    //limpando o conteudo do tbody
+    tbody.innerText = "";
 
     //faz uma requisao na url da API e usando o GET para pegar uma lista de objetos
     fetch("http://localhost:4000/produtos")
 
         // Convetendo os dados (lista de produtos) de texto json para objeto json
         .then(response => response.json())
-        .then(json => {
-            json.forEach(produto => {
+        .then(produtos => {
+            produtos.forEach(produto => {
                 // criando novos elemntos como o tr,th e tdnome tdpreco
                 tr = document.createElement('tr');
                 th = document.createElement('th');
@@ -74,7 +92,8 @@ const ler = () => {
 
                 // adicionado evento de deletar no botao de deletar
                 icon_deletar.onclick = () => {
-                    console.log(produto.id);
+                
+                    deletar(produto.id);
                 }
 
                 //inserido os filhos do elemento do tr
@@ -91,7 +110,35 @@ const ler = () => {
         });
 }
 
-const cadastrar = (id, nome, preco) => {
+const salvarProduto = () => {
+    //pegendo os elementos input da tela
+    const inputNome = document.getElementById('nome');
+    const inputPreco = document.getElementById('preco');
+
+    //verificando se os valores dos inputs sao validos
+    if (nome.value && preco.value) {
+        //pegando o id da url
+        const id_produto = getidbyURL();
+
+        //verificando se existe um id na url
+        (id_produto) ? 
+        //se sim editamos 
+        editar(id_produto, inputNome.value, inputPreco.value)
+        //se nao, cadastramos 
+        :cadastrar(inputNome.value, inputPreco.value);
+
+        
+        
+    } else {
+        //caso os campos estejam invalidos, mostrar mensagem na tela
+        alert("Campos obrigatórios");
+    }
+
+
+}
+
+
+const cadastrar = (nome, preco) => {
     // POST request using fetch()
     fetch("http://localhost:4000/produtos", {
 
@@ -100,7 +147,6 @@ const cadastrar = (id, nome, preco) => {
 
         // Adding body or contents to send
         body: JSON.stringify({
-            id: id,
             nome: nome,
             preco: preco
         }),
@@ -147,41 +193,27 @@ const editar = (id, nome, preco) => {
         .then(json => window.location.replace('tabela.html'));
 }
 
-const getidbyURL = () => {
-    //pegando a url atual
-    const url_string = window.location.href;
-    //coventendo a string de url em um objeto URL
-    const url = new URL(url_string);
-    //pegando o parametro ID da url
-    const id_produto = url.searchParams.get("id");
+const deletar = (idProduto) =>{
+    const confirm = window.confirm("tem certeza que deseja deletar?");
+    if (!confirm)return false;
 
-    //retornado id de produto
-    return id_produto;
-} 
+     // contatenando o id na url para fazer o PUT
+     fetch("http://localhost:4000/produtos/" + idProduto, {
 
-const getbyid = () => {
-    const inputNome = document.getElementById('nome');
-    const inputPreco = document.getElementById('preco');
-    const inputId = document.getElementById('id');
+     // chamando o metodo DELETE para deletar dado
+     method: "DELETE",
 
-    const id_produto = getidbyURL();
+     // Adding headers to the request
+     headers: {
+         "Content-type": "application/json; charset=UTF-8"
+     }
+ })
 
-    fetch("http://localhost:4000/produtos/"+id_produto)
+     // Converting to JSON
+     .then(response => response.json())
 
-        // Convetendo os dados (lista de produtos) de texto json para objeto json
-        .then(response => response.json())
-        .then(produto => {
-
-            //inserido os valores dos inputs de acordo com o produto
-            // recebido da requisição
-            inputNome.value = produto.nome;
-            inputPreco.value = produto.preco;
-            inputId.value = produto.id
-            
-            // desabilitado o campo id para nao permitir editar
-            inputId.disabled = true;
-        })
-
+     // chamado o metodo ler para atualizar os itens da tabela
+     .then(json => ler());
 }
 
 window.addEventListener('DOMContentLoaded', event => {
